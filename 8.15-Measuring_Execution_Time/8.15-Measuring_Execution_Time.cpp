65d9b1a9ef564abee5ad4f48b35f48051bbe7b93
unm7925 <unm7925@gmail.com>
2024-12-03 20:42:57 +0900

    Add << Measuring_Execution_Time

---
+++ 8.15-Measuring_Execution_Time/8.15-Measuring_Execution_Time.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using second_t = std::chrono::duration<double, std::ratio<1>>;
+
+    std::chrono::time_point<clock_t> start_time = clock_t::now();
+
+  public:
+    void elapsed()
+    {
+        std::chrono::time_point<clock_t> end_time = clock_t ::now();
+
+        cout << std::chrono::duration_cast<second_t>(end_time - start_time).count() << endl;
+    }
+};
+
+int main()
+{
+    random_device rnd_device;
+    mt19937 mersenne_engine{rnd_device()};
+
+    vector<int> vec(100000);
+
+    for (unsigned int i = 0; i < vec.size(); ++i)
+    {
+        vec[i] = i;
+    }
+
+    std::shuffle(begin(vec), end(vec), mersenne_engine);
+
+    // 검사
+    /*
+    for (auto &e : vec)
+        cout << e << " ";
+
+    cout << endl;
+    */
+
+    Timer timer;
+
+    std::sort(begin(vec), end(vec));
+
+    timer.elapsed();
+
+    /* 검사
+    for (auto &e : vec)
+        cout << e << " ";
+
+    cout << endl;
+    */
+
+    return 0;
+}
